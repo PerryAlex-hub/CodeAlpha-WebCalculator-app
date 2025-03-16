7a82b31fede3ccb669bd41ae42fd7a08b13ecc33
Perry Alex <ifechinwokedi@gmail.com>
2025-03-16 01:04:15 +0100

    Stored the last operation, added delete button and added the rounding of numbers function

--- calculator.js
+++ calculator.js
@@ -1,10 +1,36 @@
-let calculate = ' ';
+let calculate = localStorage.getItem('calculate') || ' ';
 
 function calculation (value) {
     calculate += value
     document.querySelector('.js-input').innerHTML = calculate;
-    
+    save()
     return;
+  
+}
+
+
+function evaluateResult () {
+  let result = Number(eval(calculate))
+
+  if (result % 1 === 0) {
+    document.querySelector('.js-input').innerHTML  = result
+  }
+  else{
+    document.querySelector('.js-input').innerHTML  = result.toFixed(2)
+  }
+  calculate = " "
+  save()
 
 }
 
+function deleteBtn() {
+  calculate = calculate.slice(0, -1);
+  document.querySelector('.js-input').innerHTML = calculate || "0"
+  save()
+}
+
+function save() {
+  localStorage.setItem('calculate',calculate)
+}
+
+document.querySelector('.js-input').innerHTML = calculate || '0'
